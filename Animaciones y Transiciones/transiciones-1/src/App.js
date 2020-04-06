fd858f984871e5f81bea40d96c0dbcf0ec2d9bea
Zoila <zoilavillatoro6694@outlook.com>
2020-04-06 08:42:16 -0600

    added init transition

---
+++ Animaciones y Transiciones/transiciones-1/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const Header = ({ show }) => {
+
+  const activeStyles = {
+    background: '#3d1f9a',
+    transform: 'scale(1)',
+    color: 'white'
+  }
+
+  let headerStyles = {
+    background: 'black',
+    transform: 'scale(0)',
+    position: 'absolute',
+    textAlign: 'center',
+    borderRadius: '.4em',
+    color: 'white',
+    padding: '0.5em',
+    margin: '0.5em',
+    fontSize: '14px',
+    transition: 'all 800ms ease'
+  }
+
+  if (show) {
+     headerStyles = {
+       ...headerStyles,
+       ...activeStyles
+     }
+   }
+
+  return (
+    <header style={headerStyles}>
+      <h1>
+        Transiciones CSS en linea
+        <span role='img' aria-label='fire'>
+          🔥
+        </span>
+      </h1>
+    </header>
+  )
+}
+
+const App = () => {
+  const [active, setActive] = useState(false)
+
+  const toggle = () => setActive(!active)
+
+  return (
+    <div>
+      {/*<button onClick={toggle}>
+        { active ? 'Desactivar' : 'Activar' }
+        </button>
+        <Header show={active} />*/}
+      <button onClick={toggle}>
+        {active ? 'Desactivar' : 'Activar'}
+      </button>
+      <Header show={active} />
+    </div>
+  )
+}
+export default App
